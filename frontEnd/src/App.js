17ef5c2f0b8b7fcb3b37966db1351bc3e5069cc3
chris-malloy <chrismalloymusic@gmail.com>
2017-12-15 13:01:21 -0500

    added google maps component

--- frontEnd/src/App.js
+++ frontEnd/src/App.js
@@ -3,6 +3,7 @@ import logo from './logo.svg';
 import './App.css';
 import Countries from './containers/Countries';
 import CountryAdd from './containers/CountryAdd';
+import MyFancyComponent from './containers/Map';
 
 class App extends Component {
   render() {
@@ -10,6 +11,7 @@ class App extends Component {
       <div className="App">
         <Countries />
         <CountryAdd />
+        <MyFancyComponent />
       </div>
     );
   }
